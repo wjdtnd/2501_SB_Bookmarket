259b6af5392c317522ca14ea69a87f54cdfbc7f8
wjdtnd <gkssk15963@gmail.com>
2025-05-28 11:47:07 +0900

    250528 도서

---
+++ src/main/java/kr/ac/kopo/lhn/bookmarket/controller/loginController.java
@@ -0,0 +1,23 @@
+package kr.ac.kopo.lhn.bookmarket.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class loginController {
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+    @GetMapping("/loginfailed")
+    public String loginfailed(Model model) {
+        model.addAttribute("error", true);
+        return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout() {
+        return "login";
+    }
+}
